9102c1bfe271ab59b4202d65eac79a92ef2aa233
Eduardo <eduardocastrogt@gmail.com>
2019-04-26 12:32:11 -0600

    Login

--- app/src/main/java/com/ecastro/appguatemala/LoginActivity.java
+++ app/src/main/java/com/ecastro/appguatemala/LoginActivity.java
@@ -1,15 +1,62 @@
 package com.ecastro.appguatemala;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import  visual.UI;
 
 public class LoginActivity extends AppCompatActivity {
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        //Pantalla completa
+        UI.HideUI(this);
+    }
+
+    @Override
+    protected void onPause(){
+        super.onPause();
+
+        //Pantalla completa
+        UI.HideUI(this);
+    }
+
+    @Override
+    protected void onResume(){
+        super.onResume();
+
+        //Pantalla completa
+        UI.HideUI(this);
+    }
+
+    @Override
+    protected void onStart(){
+        super.onStart();
+
+        //Pantalla completa
+        UI.HideUI(this);
+    }
+
+    @Override
+    protected void onStop(){
+        super.onStop();
+
+        //Pantalla completa
+        UI.HideUI(this);
+    }
+
+    @Override
+    protected void onRestart(){
+        super.onRestart();
+
+        //Pantalla completa
+        UI.HideUI(this);
     }
 }
--- app/src/main/java/com/ecastro/appguatemala/TrashActivity.java
+++ app/src/main/java/com/ecastro/appguatemala/TrashActivity.java
@@ -1,13 +1,31 @@
 package com.ecastro.appguatemala;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class TrashActivity extends AppCompatActivity {
+public class TrashActivity extends AppCompatActivity implements View.OnClickListener{
+
+    private Button btnLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_trash);
+
+        btnLogin = (Button)findViewById(R.id.botonlogin);
+        btnLogin.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        if(v.getId() == btnLogin.getId()){
+            Intent i = new Intent(TrashActivity.this,LoginActivity.class);
+            startActivity(i);
+            finish();
+        }
     }
 }
