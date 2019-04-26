fd3ae0ac0b24a1ac863e75000a1348d492b8b5be
Eduardo <eduardocastrogt@gmail.com>
2019-04-26 10:58:48 -0600

    Fonts

---
+++ app/src/main/java/com/ecastro/appguatemala/LoginActivity.java
@@ -0,0 +1,15 @@
+package com.ecastro.appguatemala;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import  visual.UI;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
--- app/src/main/java/com/ecastro/appguatemala/SplashScreenActivity.java
+++ app/src/main/java/com/ecastro/appguatemala/SplashScreenActivity.java
@@ -1,11 +1,15 @@
 package com.ecastro.appguatemala;
 
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import visual.UI;
 
 public class SplashScreenActivity extends AppCompatActivity {
+
+    private static int SPLASH_TIME_OUT = 2000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,6 +18,16 @@ public class SplashScreenActivity extends AppCompatActivity {
 
         //Pantalla completa
         UI.HideUI(this);
+
+        //Ventana temporal
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(SplashScreenActivity.this,TrashActivity.class);
+                startActivity(i);
+                finish();
+            }
+        },SPLASH_TIME_OUT);
     }
 
     @Override
